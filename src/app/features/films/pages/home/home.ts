7c022a44040087d18f5b28cbad337814203e66a9
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-29 02:32:04 +0400

    feat: implement filtering films

--- src/app/features/films/pages/home/home.ts
+++ src/app/features/films/pages/home/home.ts
@@ -1,23 +1,25 @@
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, computed, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
 import { Autofocus } from "../../../../shared/directives/autofocus";
 import { Film } from '../../service/film';
 import { FilmCard } from "../../components/film-card/film-card";
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule, Autofocus, FilmCard, RouterLink],
+  imports: [FormsModule, Autofocus, FilmCard, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
 
-  private fb = inject(FormBuilder);
   private filmService = inject(Film)
-  public films = this.filmService.films;
-  public filterForm = this.fb.group({
-    search: [''],
+  private filmsUnfiltered = this.filmService.films;
+
+  public filter = signal('');
+  public films = computed(() => {
+    return this.filmsUnfiltered().filter(film => film.title.toLowerCase().includes(this.filter().toLowerCase()));
   });
 }
