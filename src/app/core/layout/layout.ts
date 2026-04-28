60b00eec76a3fef6fff22cae9f572972131aa1e1
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-28 19:55:56 +0400

    feat: add footer

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Breadcrumb } from "./breadcrumb/breadcrumb";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet, Breadcrumb],
+  imports: [Header, RouterOutlet, Breadcrumb, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
