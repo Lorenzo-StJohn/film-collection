50d32d0fbaae76a14974c4b38f342180b086cd0c
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-28 14:59:44 +0400

    feat: add header

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [Header, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
