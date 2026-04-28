99b739676040f13d2899faa7753576f74416779c
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-28 17:15:01 +0400

    feat: add about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about').then((module) => module.About),
+  },
+];
