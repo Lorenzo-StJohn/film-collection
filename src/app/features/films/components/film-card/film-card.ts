9f1243ba24441b199a8e17a0abb9cd3c53f17271
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-29 12:29:57 +0400

    feat: add skeleton for film card

--- src/app/features/films/components/film-card/film-card.ts
+++ src/app/features/films/components/film-card/film-card.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgClass } from '@angular/common';
 
@@ -13,6 +13,7 @@ import { FilmInterface } from '../../models/film';
 export class FilmCard {
   public film = input.required<FilmInterface>();
   public favToggle = output<boolean>();
+  public posterLoaded = signal(false);
 
   public onClick() {
     this.favToggle.emit(!this.film().isFavorite);
